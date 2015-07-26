cae7c5d13db8ecdc1e8cc9c438bde013fbc0e2e2
lexandro <lexandro2000@gmail.com>
2015-07-26 13:32:23 +0200

    Adding example in the comments to dockerBuild

--- src/main/groovy/dockerBuild.groovy
+++ src/main/groovy/dockerBuild.groovy
@@ -5,29 +5,43 @@ import javaposse.jobdsl.dsl.helpers.step.*
  * Simple PoC method that take a string and a number and add and echo shell command as job step.
  *
  * Example:
- *   job('jobName') {
- *     steps {
- *       echo('some text', 123)
- *     }
- *   }
+ *
+ * job('dockerBuildJob') {
+ *   steps {
+ *       dockerBuild('tcp://docker.host.test',
+ *               'docker_host',
+ *               'http://regisry.url',
+ *               'registry_credentials',
+ *               'testRepo',
+ *               'testTag',
+ *               true,
+ *               true,
+ *               true,
+ *               true,
+ *               true,
+ *               true,
+ *               true,
+ *               'docker_dir')
+ *    }
+ * }
  */
 StepContext.metaClass.dockerBuild = { String serverUri,
                                       String serverCredentialsId,
                                       String registryUrl,
                                       String registryCredentialsId,
-                                      String repoName,
-                                      String repoTag,
-                                      Boolean skipPush,
-                                      Boolean noCache,
-                                      Boolean forcePull,
-                                      Boolean skipBuild,
-                                      Boolean createFingerPrint,
-                                      Boolean skipDecorate,
-                                      Boolean skipTagLatest,
-                                      String dockerFilePath ->
+                                      String repositoryName,
+                                      String repositoryTag,
+                                      Boolean skipPushToRegistry,
+                                      Boolean noCacheForBuild,
+                                      Boolean forcePullImage,
+                                      Boolean skipBuildImage,
+                                      Boolean createFingerPrintForImage,
+                                      Boolean skipDecorateBuildName,
+                                      Boolean skipToTagLatest,
+                                      String pathToDockerfile ->
     stepNodes << new NodeBuilder().'com.cloudbees.dockerpublish.DockerBuilder' {
-        'repoName' repoName
-        'repoTag' repoTag
+        'repoName' repositoryName
+        'repoTag' repositoryTag
         'server' {
             'uri' serverUri
             'credentialsId' serverCredentialsId
@@ -37,15 +51,14 @@ StepContext.metaClass.dockerBuild = { String serverUri,
             'credentialsId' registryCredentialsId
 
         }
-        'skipPush' skipPush
-        'noCache' noCache
-        'forcePull' forcePull
-        'skipBuild' skipBuild
-        'createFingerPrint' createFingerPrint
-        'skipDecorate' skipDecorate
-        'skipPush' skipPush
-        'skipTagLatest' skipTagLatest
-        'dockerFilePath' dockerFilePath
+        'skipPush' skipPushToRegistry
+        'noCache' noCacheForBuild
+        'forcePull' forcePullImage
+        'skipBuild' skipBuildImage
+        'createFingerPrint' createFingerPrintForImage
+        'skipDecorate' skipDecorateBuildName
+        'skipTagLatest' skipToTagLatest
+        'dockerFilePath' pathToDockerfile
 
     }
 }
